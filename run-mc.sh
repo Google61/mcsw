cae3bfd631316a496607b0f1161573f34c852ab0
Google61 <golubev634@gmail.com>
2021-01-13 22:56:27 +0300

    Support Forge

--- run-mc.sh
+++ run-mc.sh
@@ -1,11 +1,4 @@
-customjar=fabric-server-launch.jar
-
 cd server
+. ../setup-auto-mc.cfg
 echo "eula=true">eula.txt
-if test -f "$customjar";
-then
-  jar="$customjar"
-else
-  jar=server.jar
-fi
 java -Xmx1024M -Xms1024M -jar "$jar" nogui
