bab953a7a43ea6f6b1f4efc9d59cb5d5580fb6f6
Google61 <golubev634@gmail.com>
2021-01-13 23:17:39 +0300

    Delete setup-zerotier.sh

--- setup-zerotier.sh
+++
@@ -1,4 +0,0 @@
-scriptpath="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-. $scriptpath/setup-zerotier.cfg
-curl -s https://install.zerotier.com | sudo bash
-sudo zerotier-cli join $zerotiernetid
