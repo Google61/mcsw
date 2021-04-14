7af1c6456b005d4e09220e41d3ca4526e2dbe622
Google61 <golubev634@gmail.com>
2021-04-15 00:46:35 +0300

    Fix choosing ngrok region, again

--- setup-ngrok.sh
+++ setup-ngrok.sh
@@ -2,4 +2,4 @@ wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip
 chmod +x ngrok
 ./ngrok authtoken $ngrokauthtoken
-./ngrok tcp --region=$NGROKREGION 25565
+./ngrok tcp --region=$INPUT_NGROKREGION 25565
