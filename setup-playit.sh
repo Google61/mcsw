86bf8a4a6f96c4ef07a6d1fa01f4933a22793df1
Google61 <golubev634@gmail.com>
2021-04-26 23:23:44 +0300

    Update playit

--- setup-playit.sh
+++ setup-playit.sh
@@ -1,6 +1,6 @@
-wget -q https://playit.gg/downloads/playit-linux_64-0.2.0-beta-rc-3
-chmod +x playit-linux_64-0.2.0-beta-rc-3
-./playit-linux_64-0.2.0-beta-rc-3 2>url.log &
+wget -q https://playit.gg/downloads/playit-linux_64-0.3.12
+chmod +x playit-linux_64-0.3.12
+./playit-linux_64-0.3.12 2>url.log &
 sleep 5s
 echo "Click the link below!"
 grep -io 'https://playit.gg/claim/v2/[a-z0-9]*' url.log
