29fd61be06e73cfe09e4fbe268625c0acb5b9ce9
Google61 <golubev634@gmail.com>
2021-04-26 22:14:50 +0300

    Quiet wget

--- setup-ngrok.sh
+++ setup-ngrok.sh
@@ -1,4 +1,4 @@
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip
 chmod +x ngrok
 ./ngrok authtoken $ngrokauthtoken
