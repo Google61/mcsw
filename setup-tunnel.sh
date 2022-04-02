fd8461ec2ef2eb44a15d4810ab75f3ebd7f9c39a
Google61 <golubev634@gmail.com>
2022-04-02 22:59:28 +0300

    Update setup-tunnel.sh

--- setup-tunnel.sh
+++ setup-tunnel.sh
@@ -5,14 +5,16 @@ wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip
 chmod +x ngrok
 ./ngrok authtoken $ngrokauthtoken
-./ngrok tcp --region=$ngrokregion $port
+./ngrok tcp --region=$ngrokregion $port &
+sleep 2s
+echo "ngrok URL: $(curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url)"
 ;;
 playit)
 curl -SsL https://playit-cloud.github.io/ppa/key.gpg | sudo apt-key add -
 sudo curl -SsL -o /etc/apt/sources.list.d/playit-cloud.list https://playit-cloud.github.io/ppa/playit-cloud.list
 sudo apt update
 sudo apt install playit
-playit -s > url.log
+playit -s > url.log &
 sleep 2s
 echo "Claim URL: $(grep -io 'https://playit.gg/claim/[a-z0-9]*' url.log)"
 ;;
